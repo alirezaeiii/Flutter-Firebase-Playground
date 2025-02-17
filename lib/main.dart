638abe260bd413a592c06c84d990473a4150c09f
Ali <m.ali.rezaeiii09@gmail.com>
2025-02-17 22:52:48 +0330

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:dart/view/login_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginView(),
+    ),
+  );
+}
