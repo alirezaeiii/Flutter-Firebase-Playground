6b98a94af10643a8be8600b4fe90d1aa9141ec7d
Ali <m.ali.rezaeiii09@gmail.com>
2025-02-18 15:29:41 +0330

    Add email verification and connect login and register views

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:dart/firebase_options.dart';
 import 'package:dart/view/login_view.dart';
+import 'package:dart/view/register_view.dart';
+import 'package:dart/view/verified_email.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +15,41 @@ void main() {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView()
+      },
     ),
   );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('User email is verified');
+              } else {
+                return const VerifiedEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
 }
