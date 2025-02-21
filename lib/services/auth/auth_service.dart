b17de3ba1ce029703d59423cb4e09cfc9cda36a9
Ali <m.ali.rezaeiii09@gmail.com>
2025-02-21 23:35:33 +0330

    Add Auth Providers

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,37 @@
+import 'package:dart/services/auth/auth_provider.dart';
+import 'package:dart/services/auth/auth_user.dart';
+
+class AuthService implements AuthProvider {
+  final AuthProvider provider;
+
+  AuthService(this.provider);
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) =>
+      provider.createUser(
+        email: email,
+        password: password,
+      );
+
+  @override
+  AuthUser? get currentUser => provider.currentUser;
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) =>
+      provider.logIn(
+        email: email,
+        password: password,
+      );
+
+  @override
+  Future<void> sendVerificationEmail() => provider.sendVerificationEmail();
+
+  @override
+  Future<void> signOut() => provider.signOut();
+}
