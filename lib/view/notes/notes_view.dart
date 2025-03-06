88c4ecb08f8c8a94a3507d5f1dc33837fd301a92
Ali <m.ali.rezaeiii09@gmail.com>
2025-03-06 19:01:43 +0330

    Rename

--- lib/view/notes/notes_view.dart
+++ lib/view/notes/notes_view.dart
@@ -26,7 +26,7 @@ class _NotesViewState extends State<NotesView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Main UI'),
+        title: const Text('My Notes'),
         actions: [
           IconButton(
             onPressed: () {
