11d62076c065bc63701953feff45734ec3762d04
Ali <m.ali.rezaeiii09@gmail.com>
2025-03-07 19:58:19 +0330

    Add padding

--- lib/view/notes/create_update_notes_view.dart
+++ lib/view/notes/create_update_notes_view.dart
@@ -116,12 +116,15 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
               _setupTextControllerListener();
-              return TextField(
-                controller: _textController,
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                decoration: InputDecoration(
-                  hintText: context.loc.start_typing_your_note,
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: _textController,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    hintText: context.loc.start_typing_your_note,
+                  ),
                 ),
               );
             default:
